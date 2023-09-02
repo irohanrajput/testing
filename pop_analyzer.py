90d51e02782d27cca786f576b294f87fa153763d
irohanrajput <itusharsingh27@gmail.com>
2023-09-03 02:20:42 +0530

    tryna get hang of pop and some string methods

---
+++ pop_analyzer.py
@@ -0,0 +1,16 @@
+a = [1,2,3,4,5]
+punc = 2
+
+def analyze():
+    for char in a:
+        ind = a.index(char)
+        if char == punc:
+            a.pop(ind)
+    return (a)
+
+analyze()
+
+print(a)
+    
+
+
